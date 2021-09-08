d03f538672e56904d3a288a7caf1aad8ae83bbfb
soybeanboy15 <53890241+soybeanboy15@users.noreply.github.com>
2021-09-08 19:25:56 +1000

    Update pid.py

--- ColourTrackingPanTilt/pid.py
+++ ColourTrackingPanTilt/pid.py
@@ -1,14 +1,3 @@
-# -*- encoding: utf-8 -*-
-'''
-
- _____     _        _____     _       _
-|   __|___| |___   | __  |___| |_ ___| |_
-|   __|  _| | -_|  |    -| . | . | . |  _|
-|_____|_| |_|___|  |__|__|___|___|___|_|
-
-@author: Víctor Mayoral Vilches <victor@erlerobot.com>
-@description: PID controller class.
-'''
 import time
 
 class PID:
